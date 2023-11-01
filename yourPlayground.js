e761d4123c75d52edc12e60034f5acc513370648
Adarsh Tiwari <gautamtiwary79@gmail.com>
2023-11-01 21:33:32 +0530

    User input code commit

---
+++ yourPlayground.js
@@ -0,0 +1,28 @@
+console.log('Hello World')
+console.log("Adarsh Tiwari");
+
+// Variable Description 
+Name = 'Adarsh'
+console.log(Name)
+
+sentence="How are you doing today!!"
+console.log(sentence)
+
+// What are Operators 
+// + - * / **
+
+food = 20
+tipPercentage= 0.2
+tipAmount=food*tipPercentage
+
+console.log(tipAmount)
+
+// How to take input from a user?
+
+const prompt = require('prompt-sync')()
+
+food_2 = prompt('What is the cost of the item? ')
+tipPercentage_2= 0.2
+tipAmount_2=food_2*tipPercentage_2
+
+console.log(tipAmount_2)
